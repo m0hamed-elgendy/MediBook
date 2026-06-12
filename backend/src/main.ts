dfbcbe72b449a82a910de09f091a18fd19a979a7
Mohamed Elgendy <moellgendy@gmail.com>
2026-06-13 02:28:57 +0300

    feat: enable CORS for all origins

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,6 +5,10 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
 
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+  origin: '*',
+  credentials: true,
+});
   app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true,
